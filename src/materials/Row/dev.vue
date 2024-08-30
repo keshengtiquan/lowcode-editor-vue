08baab064624b443d1e38b394373322c58cd4f19
dadaz <keshengtiquan@163.com>
2024-08-30 09:47:32 +0800

    添加表格组件插槽

---
+++ src/materials/Row/dev.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-row :data-component-id="id" :style="styles" class="w-full border-[1px] border-[#000] min-h-[100px]">
+  </a-row>
+</template>
+<script setup lang="ts">
+import {CSSProperties} from "vue/dist/vue";
+
+defineProps<{
+  id: number;
+  styles: CSSProperties;
+  columns: any[];
+}>();
+</script>
+
+<style scoped>
+
+</style>
